9f23396c100cf32422b74f159ce2693c57d19b3e
RampantOwl <trygve@grimnes.org>
2014-12-06 00:09:13 +0100

    Tightened up.

--- paylane.js
+++ paylane.js
@@ -1,35 +1,11 @@
 var util = require( 'util' )
 var request = require( 'request' )
 
+var helpers = require( './helpers' )
 var PAYLANE_URL = "https://%s:%s@direct.paylane.com/rest/%s"
 
 var username = null
 var password = null
-
-function setCredentials( user , pass){
-	username = user
-	password = pass
-}
-
-function createRoute( pathArray ){
-	return function( paylaneParameters , callback ){
-		var route = pathArray.join("/")
-		var options = {
-			url : util.format(PAYLANE_URL, username , password , route ),
-			method : 'post',
-			json : true,
-			body : paylaneParameters			
-		}
-			send( options , callback )
-	}
-}
-
-function send( options , callback ){
-	request( options, function( err , response, body ){
-		callback( err , body )
-	})	
-}
-
 
 var resources = []
 resources["cards"] = [
@@ -39,7 +15,7 @@ resources["cards"] = [
 	"generateToken",
 	"check",
 	"checkByToken"
-	]
+]
 resources["paypal"] = [
 	"sale",
 	"authorization",
@@ -53,23 +29,14 @@ resources["sofort"] = [
 	"sale"
 ]
 resources["refunds"] = ""
-					
-var routes = []
-
-Object.keys(resources).forEach( function( key , i , a){
-	if( typeof( resources[a[i]] ) != "object" ){
-		routes[key] = createRoute( [ key ]  )
-	}else{
-	resources[a[i]].forEach( function(childKey , childIndex , childArray ){
-		if( routes[key] == undefined )
-			routes[key] = new Array()
-		
-		routes[key][childKey] = createRoute( [key , childKey] )
-	})
-	}
-})
 
 
-routes.setCredentials = setCredentials
+
+routes = helpers.transformResourceListToRoute( resources )
+
+routes.setCredentials = function( user , pass){
+	username = user
+	password = pass
+}
 
 module.exports = routes
