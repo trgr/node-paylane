b1e0458dc3a2a32ec6e3788c4d971500c5aeaa6a
RampantOwl <trygve@grimnes.org>
2014-12-05 23:31:06 +0100

    Added rest of API

--- paylane.js
+++ paylane.js
@@ -18,40 +18,55 @@ function send( options , callback ){
 }
 
 
-var resources = {
-	"cards" : [
+var resources = []
+resources["cards"] = [
 	"saleByToken",
 	"authorizationByToken",
 	"authorization",
 	"generateToken",
 	"check",
 	"checkByToken"
-	],
-	"paypal" : [
-		"sale",
-		"authorization",
-		"stopRecurring"
 	]
+resources["paypal"] = [
+	"sale",
+	"authorization",
+	"stopRecurring"
+]
+resources["directdebits"] = [
+	"sale"
+]
+
+resources["sofort"] = [
+	"sale"
+]
+resources["refunds"] = ""
+					
+function createRoute( pathArray ){
+	return function( paylaneParameters , callback ){
+		var route = pathArray.join("/")
+		var options = {
+			url : util.format(PAYLANE_URL, username , password , route ),
+			method : 'post',
+			json : true,
+			body : paylaneParameters			
+		}
+			send( options , callback )
+	}
 }
 
 var routes = []
 
 Object.keys(resources).forEach( function( key , i , a){
+	if( typeof( resources[a[i]] ) != "object" ){
+		routes[key] = createRoute( [ key ]  )
+	}else{
 	resources[a[i]].forEach( function(childKey , childIndex , childArray ){
 		if( routes[key] == undefined )
 			routes[key] = new Array()
 		
-		routes[key][childKey] = function( paylaneParameters , callback ){
-			var route = util.format("%s/%s" , key ,childKey )
-			var options = {
-				url : util.format(PAYLANE_URL, username , password , route ),
-				method : 'post',
-				json : true,
-				body : paylaneParameters			
-			}
-			send( options , callback )
-		}
+		routes[key][childKey] = createRoute( [key , childKey] )
 	})
+	}
 })
 
 
