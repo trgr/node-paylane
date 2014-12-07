6dd671960e5d1d215e2c887037c390e252365f9a
RampantOwl <trygve@grimnes.org>
2014-12-07 13:40:00 +0100

    Tweaked the autogeneration

--- paylane.js
+++ paylane.js
@@ -5,69 +5,76 @@ var util = require( 'util' )
 var request = require( 'request' )
 var helpers = require( './helpers' )
 
-var resources = []
+var PAYLANE_URL = "https://%s:%s@direct.paylane.com/rest/%s"
 
-resources["cards"] =[
-		"saleByToken",
-		"authorizationByToken",
-		"authorization",
-		"generateToken",
-		"check",
-		"checkByToken"
-]
-resources["paypal"] = [
-	"sale",
-	"authorization",
-	"stopRecurring"
-]
-resources["directdebits"] = [
-	"sale"
-]
+var username = null
+var password = null
 
-resources["sofort"] = [
-	"sale"
-]
-resources["refunds"] = ""
+/* 
+ * The following config is transformed into an object which is then exported as this module.
+ * If the hash element is an array, the array will be transformed to an object where each element becomes a key potinting to a function of the same name on the paylane webservice.
+ * If the hash element is null, then it will point to a function of the same name on the paylane webservice.
+ * If the hash element is a function, then it is kept as-is.
+ *
+ * The functions generated take two parameters, a paylane configuration object, and a callback.
+ * The callback is a function with the standard error first function( err, succes)
+ * 
+ * This module can then be used like: paylane.cards.saleByToken( params, function( err , success ) {} )
+ */
 
-var resourceEndpoints = Object.keys( resources )
-var routes = []
-resourceEndpoints.forEach( function( endpoint ){	
-	if( typeof( endpoint) != "object" ){
-		routes[endpoint] = helpers.createRoute( [ endpoint ] )
-	}else{
-		if( typeof( routes[endpoint] ) == undefined )
-			routes[endpoint] = new Array()
-		
-		routes[endpoint] = resources[endpoint].map( function( current , index ){
-			
-			if( typeof( current ) == "function" )
-				return current
-			
-		if( typeof( current) == "object" )
-			return helpers.createRoute( [ endpoint , current] )
-		})
-	}
-})
-
-
-/*var routes = resources.map( function( current , index , array ){
-	return current
-	if( typeof( current ) == "function" )
-		return current
-	
-		if( typeof( current) == "object" ){
-		
-		current.map( function( childCurrent , childIndex , childArray ){
-			
-			return crateRoute( [current , childCurrent ] )
-		})
-	}
-})*/
-console.log( routes )
-
-routes.setCredentials = function( user , pass){
+var setCredentials = function( user , pass){
 	username = user
 	password = pass
 }
+
+var routeConfig = {
+	"cards" : [ "saleByToken" , "authorizationByToken" , "authorization", "generateToken", "check", "checkByToken" ],
+	"paypal" : [	"sale", "authorization", "stopRecurring"],
+	"directdebits" : [ "sale" ],
+	"sofort" : [ "sale" ],
+	"refunds" : null,
+	"authorizations" : [ "capture" , "close" ],
+	"resales" : [ "sale" , "authorization" ],
+	"threedsecure" : [ "checkCard" , "checkCardByToken" , "authSale"],
+	
+	"setCredentials" : setCredentials
+}
+
+
+
+function send( options , callback ){
+	request( options, function( err , response, body ){
+		callback( err , body )
+	})	
+}
+
+function createRoute( pathArray ){
+	return function( paylaneParameters , callback ){
+
+		var route = pathArray.join("/")
+		var options = {
+			url : util.format(PAYLANE_URL, username , password , route ),
+			method : 'post',
+			json : true,
+			body : paylaneParameters			
+		}
+		send( options , callback )
+	}
+}
+
+var routes = {}
+
+Object.keys( routeConfig).forEach(function( resource ){
+	if( routeConfig[resource] == null){
+		routes[resource] = createRoute( [ resource ] )
+	}else if( typeof( routeConfig[resource] ) == "function" ){
+		routes[resource] = routeConfig[resource]
+	}else{
+		routes[resource] = routeConfig[resource].reduce(function( last , current , index , array ){
+			last[current] = createRoute( [ resource , current ] )
+			return last
+		},{})
+	}
+})
 
 module.exports = routes
